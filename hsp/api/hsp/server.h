e5eda73ea659a35be258e6cd96cef2f9bf0a2153
LandaMm <qwer.009771@gmail.com>
2025-04-10 18:18:21 +0200

    feat: server accept listener

--- hsp/api/hsp/server.h
+++ hsp/api/hsp/server.h
@@ -1,28 +1,40 @@
 
 #pragma once
+#include <functional>
 #include<netdb.h>
 
 #include "hsp/address.h"
 #include "hsp/connection.h"
+#include "hsp/request.h"
+#include "hsp/response.h"
+
+#define SERVER_RECV_BUFFER_SIZE 1024
+#define SERVER_BACKLOG 10
 
 namespace HSP
 {
     class Server
     {
+    public:
+        using Listener = std::function<Response*(Request*)>;
     private:
         Address m_addr;
         int m_server;
+        Listener m_listener;
     public:
         Server(const char* host, const char* port, const addrinfo* hints);
-    public:
+    private:
         void Bind();
         void Listen(int n);
         Connection Accept();
-    public:
+    private:
         void Close();
         void Shutdown(int how);
     public:
         inline Address GetAddr() const { return m_addr; }
+    public:
+        void Stop();
+        void Start(Listener listener);
     };
 }
 
