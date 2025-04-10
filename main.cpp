cdbf0416c32fd8e6c5f8614c61b3242228f3a28a
LandaMm <qwer.009771@gmail.com>
2025-04-10 16:28:53 +0200

    feat: update server to accept packets

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include<netdb.h>
 #include<sys/socket.h>
 
+#include "hsp/reader.h"
 #include"hsp/server.h"
 
 int main()
@@ -25,13 +26,57 @@ int main()
         auto conn = server.Accept();
         auto addr = conn.GetAddress();
         std::cout << "INFO: Received connection from: " << addr.ToString() << std::endl;
-        char data[1024];
-        int bytes_read = conn.Recv(data, 1024);
+
+        HSP::Reader reader = HSP::Reader(1024);
+        auto buffer = reader.NewBuffer();
+
+        int bytes_read = conn.Recv(buffer, 1024);
         if (bytes_read > 0)
         {
-            std::cout << "INFO: Received " << bytes_read << " bytes from connection: " << data << std::endl;
+            std::cout << "INFO: Received " << bytes_read << " bytes from connection" << std::endl;
 
-            conn.Send(data, strlen(data));
+            reader.ReadBuffer(buffer, bytes_read);
+            reader.FreeBuffer(buffer);
+
+            // TODO: read packet till the end
+            auto packet = reader.ReadPacket();
+            if (!packet)
+            {
+                std::cerr << "ERROR: Failed to decode packet" << std::endl;
+                char msg[] = "Failed to read packet";
+                conn.Send(msg, strlen(msg));
+            }
+            else
+            {
+                std::cout << "INFO: New packet received:" << std::endl;
+                std::cout << "Version:\t" << (int)packet->version << std::endl
+                          << "Flags:  \t" << (int)packet->flags << std::endl
+                          << "Header Length:\t" << packet->headers.size() << std::endl
+                          << "Payload Length:\t" << packet->payload.size() << std::endl;
+
+                std::cout << "[INFO]: Headers" << std::endl;
+                for (auto &[k, v] : packet->headers)
+                {
+                    std::cout << "'" << k << "'" << ": '" << v << "'\n";
+                }
+
+                if (packet->headers.find("Content-Encoding") != packet->headers.end())
+                {
+                    std::string contentType = packet->headers.at("Content-Encoding");
+                    if (contentType == "utf-8")
+                    {
+                        std::cout << "INFO: UTF-8 Encoded Payload:" << std::endl;
+                        char* msg = reinterpret_cast<char*>(packet->payload.data());
+                        std::cout << "'" << msg << "'" << std::endl;
+                    }
+                }
+
+                std::vector<uint8_t> data;
+                packet->Serialize(data);
+                conn.Send(data.data(), data.size());
+            }
+        } else {
+            reader.FreeBuffer(buffer);
         }
 
         std::cout << "INFO: Closing connection: " << addr.ToString() << std::endl;
@@ -42,7 +87,6 @@ int main()
     server.Close();
     server.Shutdown(SHUT_RDWR);
 
-    std::cout << ("Amir") << std::endl;
     return 0;
 }
 
