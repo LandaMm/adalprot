181de43d87c3b735a84ddb620a92bedffffc9c0b
LandaMm <qwer.009771@gmail.com>
2025-04-10 16:28:43 +0200

    feat: update client to try packets

--- client.cpp
+++ client.cpp
@@ -1,10 +1,17 @@
 
 #include"hsp/client.h"
+#include"hsp/packet.h"
+#include "hsp/reader.h"
+#include <cassert>
 #include <cerrno>
+#include <cstdlib>
 #include <cstring>
+#include <fstream>
+#include <ios>
 #include <iostream>
 #include <netdb.h>
 #include <sys/socket.h>
+#include <utility>
 
 using namespace HSP;
 
@@ -29,14 +36,65 @@ int main(int argc, char** argv)
     Client client = Client(res);
     client.Connect();
 
-    char data[] = "Hello, World!";
-    int bytes_sent = client.Send(data, strlen(data));
+    Packet *packet = new Packet();
+    packet->version = 1;
+    packet->flags = 0;
+    packet->headers.insert(std::make_pair("x-protocol", "stream"));
+    packet->headers.insert(std::make_pair("Content-Encoding", "utf-8"));
+    std::ifstream licenseFile("LICENSE", std::ios::in | std::ios::binary);
+    if (!licenseFile)
+    {
+        std::cerr << "ERROR: Failed to open 'LICENSE' file" << std::endl;
+        return 1;
+    }
+    char data[501] = {0};
+    licenseFile.read(data, 500);
+    std::streamsize charsRead = licenseFile.gcount();
+    std::cout << "DEBUG: Read " << charsRead << " characters from LICENSE\n";
+    licenseFile.close();
+    packet->payload.insert(packet->payload.end(), data, data + strlen(data));
+
+    std::vector<uint8_t> buffer;
+    packet->Serialize(buffer);
+    int bytes_sent = client.Send(buffer.data(), buffer.size());
     std::cout << "INFO: Sent " << bytes_sent << " bytes" << std::endl;
 
-    char buf[1024];
+    HSP::Reader reader = HSP::Reader(1024);
+
+    auto buf = reader.NewBuffer();
     int bytes_read = client.Recv(buf, 1024);
+
     std::cout << "INFO: Received " << bytes_read << " bytes" << std::endl;
-    std::cout << "INFO: Data: '" << buf << "'" << std::endl;
+
+    if (bytes_read > 0)
+    {
+        reader.ReadBuffer(buf, bytes_read);
+        reader.FreeBuffer(buf);
+
+        auto packet = reader.ReadPacket();
+
+        std::cout << "INFO: New Packet:" << std::endl;
+        std::cout << "Version:\t" << (int)packet->version << std::endl;
+        std::cout << "Flags:  \t" << (int)packet->version << std::endl;
+        std::cout << "Headers:" << std::endl;
+        for(auto &[k, v] : packet->headers)
+        {
+            std::cout << "\t" << '"' << k << '"' << ": " << '"' << v << '"' << std::endl;
+        }
+        std::cout << "Payload Length:\t" << packet->payload.size() << std::endl;
+        if (packet->headers.find("Content-Encoding") != packet->headers.end())
+        {
+            std::string contentType = packet->headers.at("Content-Encoding");
+            if (contentType == "utf-8")
+            {
+                std::cout << "INFO: UTF-8 Encoded Payload:" << std::endl;
+                char* msg = reinterpret_cast<char*>(packet->payload.data());
+                std::string msgStr = msg;
+                assert(msgStr == data && "DATA LOSS");
+                std::cout << "'" << msg << "'" << std::endl;
+            }
+        }
+    }
 
     client.Close();
     client.Shutdown(SHUT_RDWR);
