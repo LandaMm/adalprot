1977d1541e653ef97e2c2ca20a22c4b703a1c3f9
LandaMm <qwer.009771@gmail.com>
2025-04-13 22:45:07 +0200

    feat: predefined headers, statuses and data-formats

---
+++ hsp/api/hsp/header.h
@@ -0,0 +1,15 @@
+
+#pragma once
+
+#define H_ROUTE "route"
+#define H_STATUS "status"
+#define H_DATA_FORMAT "data-format"
+
+#define DF_TEXT "text:utf-8"
+#define DF_JSON "json:utf-8"
+
+#define S_LOST 1
+#define S_SUCC 2
+#define S_ACKN 3
+#define S_FORB 4
+
